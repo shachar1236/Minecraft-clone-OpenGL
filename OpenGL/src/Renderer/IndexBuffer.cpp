42ccd345ca24a9d2fa11bd784b0a5c4d8a5c35e2
shachar1236 <52973001+shachar1236@users.noreply.github.com>
2022-01-21 13:37:29 +0200

    Implemented IndexBuffer

--- OpenGL/src/Renderer/IndexBuffer.cpp
+++ OpenGL/src/Renderer/IndexBuffer.cpp
@@ -1,1 +1,16 @@
 #include "IndexBuffer.h"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+IndexBuffer::IndexBuffer(const float* buffer, const unsigned int& size, const unsigned int& usage)
+{
+    glGenBuffers(1, &id);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, buffer, usage);
+}
+
+IndexBuffer::~IndexBuffer() { glDeleteBuffers(1, &id); }
+
+void IndexBuffer::Bind() { glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id); }
+
+void IndexBuffer::UnBind() { glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); }
--- OpenGL/src/Renderer/IndexBuffer.h
+++ OpenGL/src/Renderer/IndexBuffer.h
@@ -1,5 +1,12 @@
 #pragma once
-class IndexBuffer
-{
-};
+class IndexBuffer {
+public:
+    IndexBuffer(const float* buffer, const unsigned int& size, const unsigned int& usage);
+    ~IndexBuffer();
 
+    void Bind();
+    void UnBind();
+
+private:
+    unsigned int id;
+};
